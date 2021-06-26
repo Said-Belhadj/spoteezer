8e72cabd763c8f6cfba63b9aaf9502ca15a3092f
Clyhtsuriva <aimeric@adjutor.xyz>
2021-06-26 21:01:16 +0200

    Moved PathValidation to the util package

--- src/main/java/musichub/main/Main.java
+++ src/main/java/musichub/main/Main.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-import static musichub.business.PathValidation.isPathValid;
+import static musichub.util.PathValidation.isPathValid;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
