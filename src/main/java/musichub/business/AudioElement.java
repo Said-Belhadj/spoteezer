2a4ae3e26bdf7b16bc16bef4211e230a64522b11
Clyhtsuriva <aimeric@adjutor.xyz>
2021-06-28 18:10:41 +0200

    Changed the type of switch used in AudioElement.java

--- src/main/java/musichub/business/AudioElement.java
+++ src/main/java/musichub/business/AudioElement.java
@@ -3,16 +3,11 @@ package musichub.business;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.UUID;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public abstract class AudioElement {
     protected String title;
@@ -106,24 +101,19 @@ public abstract class AudioElement {
     	String action = "";
     			
     	while(!action.equals("Q")) {
-    		System.out.println("P = Play \b S = Stop \b R = Reset \b Q = Quit");
-    		System.out.println("Enter your choice");
-    		action = scanner.next();
-        	action = action.toUpperCase();
-        	
-        	switch(action) {
-        		case "S" : clip.stop();
-        		break;
-        		case "P" : clip.start();
-        		break;
-        		case "R" : clip.setMicrosecondPosition(0);
-        		break;
-        		case "Q" : clip.stop();
-        		break;
-        		default : System.out.println("try again");
-        	}
-        	System.out.println("You stoped the Audio element");
-    	}
+            System.out.println("P = Play \b S = Stop \b R = Reset \b Q = Quit");
+            System.out.println("Enter your choice");
+            action = scanner.next();
+            action = action.toUpperCase();
+
+            switch (action) {
+                case "S", "Q" -> clip.stop();
+                case "P" -> clip.start();
+                case "R" -> clip.setMicrosecondPosition(0);
+                default -> System.out.println("try again");
+            }
+            System.out.println("You stoped the Audio element");
+        }
     	
     	clip.close();
     
